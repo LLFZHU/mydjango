a5be25419e139337c59f2ce5f9a9bd6c36641a3f
ZhuMX <zhumingxin4@163.com>
2019-03-27 16:41:11 +0800

    签到

--- guest/urls.py
+++ guest/urls.py
@@ -26,5 +26,7 @@ urlpatterns = [
     url(r'^accounts/login/$',views.index),
     url(r'^search_name/$', views.search_name),
     url(r'^guest_manage/$',views.guest_manage),
-    url(r'^search_guest_name/$',views.search_guest_name)
+    url(r'^search_guest_name/$',views.search_guest_name),
+    url(r'^sign_index/(?P<eid>[0-9]+)/$',views.sign_index),
+    url(r'^sign_index_action/(?P<eid>[0-9]+)/$',views.sign_index_action)
 ]
